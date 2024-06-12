6899216238c80bcc618084aa0fb86f1e4b25190b
barrosflavio <flavi011@outlook.com>
2024-06-12 15:55:34 -0300

    update

--- app.rb
+++ app.rb
@@ -1,18 +1,18 @@
 def validar_whatsapp(numero)
-  padrao = %r{^\(\d{2}\) \d \d{4}-\d{4}$}
+  padrao = /^\(\d{2}\) \d \d{4}-\d{4}$/
   padrao.match?(numero)
 end
 
 def input(question)
-  print "#{question}"
+  print question
   gets.chomp
 end
 
-numero = input("Digite seu numero no formato (00) 0 0000-0000: ")
+numero = input('Digite seu numero no formato (00) 0 0000-0000: ')
 
 if validar_whatsapp(numero)
-  puts "Número de WhatsApp válido."
+  puts 'Número de WhatsApp válido.'
   puts "Seu Whatsapp: #{numero}"
 else
-  puts "Número de WhatsApp inválido."
+  puts 'Número de WhatsApp inválido.'
 end
